43a4aa6365cc14dbdf2adb1d89834b414ef79662
EttoreBarcella <100206174+EttoreBarcella@users.noreply.github.com>
2022-05-23 17:08:16 +0200

    first commit

---
+++ vue/main.js
@@ -0,0 +1,34 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+      userInput: null,
+      todos: [
+        {
+          text: 'Andare dal veterinario',
+          completed: true,
+        },
+        {
+          text: 'Fare la spesa',
+          completed: false,
+        },
+        {
+          text: 'Pulire la camera',
+          completed: false,
+        },
+      ],
+    },
+    methods: {
+      pushTodo() {
+        if (this.userInput !== ' ') {
+          this.todos.push({
+            text: this.userInput,
+            completed: false,
+          });
+        }
+        this.userInput = '';
+      },
+      removeTodo(index) {
+        this.todos.splice(index, 1);
+      },
+    },
+  });
